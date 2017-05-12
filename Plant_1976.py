142958953507bf3a59be14027f7c49253243de74
keshengxuu <keshengxu@hotmail.com>
2017-05-12 16:53:51 -0300

    Update Plant_1976.py

--- Plant_1976.py
+++ Plant_1976.py
@@ -4,6 +4,7 @@ Created on Tue Dec 22 14:56:13 2015
 Reference:
 1.Plant, R. E., and M. Kim. "Mathematical description of a bursting pacemaker neuron by a modification of the Hodgkin-Huxley equations." Biophysical journal 16.3 (1976): 227-244.
 2. Canavier, C. C., J. W. Clark, and J. H. Byrne. "Routes to chaos in a model of a bursting neuron." Biophysical journal 57.6 (1990): 1245-1251.
+kesheng.xu@cinv.cl
 @author: ksxuu
 """
 import numpy as np
